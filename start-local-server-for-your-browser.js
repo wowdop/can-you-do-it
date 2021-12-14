79d82d733e07366a99787e4f0391acda1e1cb743
Marian Zburlea <marian@wowdop.com>
2021-12-14 13:40:48 +0000

    custom local server starter for live-server path

---
+++ start-local-server-for-your-browser.js
@@ -0,0 +1,4 @@
+console.log('WoW DoP - Rocket Boost Your Career!')
+const { exec } = require('child_process')
+exec('cd code-source && npx live-server')
+console.log('Remember to have an amazing coding experience!')
